57df396ab8b027cd7cdf4b982f083ab9cf904047
rodocimatproyect <ramirezlucarioo@gmail.com>
2025-09-24 17:17:16 -0600

    Creación de script_rodolfo y modificación en el main y del nombre.

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Script para descargar y guardar el dataset Bank Marketing en la carpeta data/.
+"""
+
+from script_rodolfo import install_package
+
+if __name__ == "__main__":
+
+    install_package("ucimlrepo")
+    
